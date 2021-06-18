eeae5dd87a803d58bc5c5987a68d18a6f16f7e22
Kevin Bein <mail@kevinbein.de>
2021-06-18 11:22:37 +0200

    fix: removed unused include dependency opencv/tracking.hpp

--- TopDownTracking/CVTests/RealsenseCameraManager.cpp
+++ TopDownTracking/CVTests/RealsenseCameraManager.cpp
@@ -3,7 +3,7 @@
 #include <random>
 #include <iostream>
 
-#include <opencv2/tracking.hpp>
+//#include <opencv2/tracking.hpp>
 #include <opencv2/calib3d.hpp>
 #include <opencv2/highgui.hpp>
 #include <utility>
